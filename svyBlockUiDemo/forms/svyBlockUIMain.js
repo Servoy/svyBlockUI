df070bea8cbb249c6dffb836b6baecd428ebb247
Paolo <paronne@servoy.com>
2018-05-10 17:29:38 +0300

    Update Sample

--- svyBlockUiDemo/forms/svyBlockUIMain.js
+++ svyBlockUiDemo/forms/svyBlockUIMain.js
@@ -9,7 +9,6 @@
  * @properties={typeid:24,uuid:"81A640BD-C24A-45F4-8B6A-56B05AFFAEE8"}
  */
 function onTabChange(previousIndex, event) {
-	elements.lblSolutionName.text = elements.tabs.getTabTextAt(elements.tabs.tabIndex);
 }
 
 /**
@@ -23,5 +22,4 @@ function onTabChange(previousIndex, event) {
  * @properties={typeid:24,uuid:"47A3340F-3CCA-4C68-87CF-D66774D6ECB8"}
  */
 function onShow(firstShow, event) {
-	onTabChange(-1, event);
 }
